c1e004e248f2710857a7e3aed3d9c62b93e9636c
jesse <jesseprescott@yahoo.com>
2022-02-20 14:46:03 -0500

    Removed irrelevant code

--- LibraryBlazor/Program.cs
+++ LibraryBlazor/Program.cs
@@ -7,8 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var apiUrl = builder.Configuration.GetSection("ApiUrl").Value;
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
 
